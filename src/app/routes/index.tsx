bb3be47ca319182e34b5e49e86d19ef9bfad9956
PedroGimenes <phgimenes.andrade@gmail.com>
2026-07-27 23:04:22 -0300

    feat; add login page and form

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Route, Routes as Switch} from "react-router-dom";
-import { Dashboard } from "../pages";
+import { Dashboard, Login } from "../pages";
 
 export const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/entrar" Component={Login}/>
         <Route path="/pagina-inicial" Component={Dashboard}/>
 
         <Route path="*" Component={() => <Redirect to="/pagina-inicial" />} />
